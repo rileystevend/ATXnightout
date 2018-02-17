b16e31cefd6950464410760ec433738d2dbff4ea
Steven Riley <rileystevend@gmail.com>
2018-02-17 14:52:31 -0600

    app successfully chooses a random show, then a random nearby restaurant

--- helpers/apirequest.js
+++ helpers/apirequest.js
@@ -1,8 +1,3 @@
-//USE BLUEBIRD MODULE TO PROMISIFY THESE FUNCTIONS?
-
-//1. function pull a show from jambase API
-//2. function pull a restaurant from zomato API
-
 const axios = require('axios');
 const jamKey = process.env.jamKey || 'jh8fu4aex27733bzszu6kprj';
 const foodKey = process.env.foodKey || 'dd071c27f3428a592bdf3cec117a2cc7';
@@ -12,7 +7,8 @@ const getPair = (zipcode, date)=> {
   return (
     axios.get(`http://api.jambase.com/events?zipCode=${zipcode}&radius=7&startDate=${date}&endDate=${date2}&page=0&api_key=${jamKey}`)
     .then((shows) => {
-      const show = shows.data.Events[0];
+      const set = shows.data.Events;
+      const show = set[Math.floor(Math.random()*set.length)];
       let long = show.Venue.Longitude.toString();
       let lat = show.Venue.Latitude.toString();
       //return whole show
@@ -22,7 +18,8 @@ const getPair = (zipcode, date)=> {
         }
       })
       .then((result2) => {
-        return [show, result2.data.nearby_restaurants[0]]
+        const places = result2.data.nearby_restaurants;
+        return [show, places[Math.floor(Math.random()*places.length)]]
       })
       .catch((err) => {
         console.log('this is error in resul2', err)
