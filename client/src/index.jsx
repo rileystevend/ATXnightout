610970df2311207f54757b5a44c6771cfd779618
Steven Riley <rileystevend@gmail.com>
2018-02-16 18:21:24 -0600

    fleshed out most of App react component

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,6 +3,42 @@ import ReactDOM from 'react-dom';
 //import Show  from [filepath]
 //import Dine component from [filepath]
 
+class App extends React.Component {
+    constructor() {
+        this.state = {
+            welcome: true,
+            show: null,
+            restaurant: null,
+        }
+        this.search = this.search.bind(this);
+    }
+
+    search(zipcode) {
+        axios.post('/app', {
+            zipcode: zipcode
+        })
+        .then((response) => {
+//DO SOMETHING WITH OUR SEARCH RESULTS
+            this.setState({
+                welcome: false
+            })
+        })
+        .catch((err) => {
+            console.log('ERROR IN THE JSX SEARCH')
+        })
+    }
+
+    render () {
+        if (this.state.welcome === true) {
+            return <h1>Welcome to the landing page!</h1>
+            //loading page
+            //<Search />
+        } else {
+            return //<Search/><Show/><Dine/>
+        }
+    }
+
+}
 
 //constructor(state: welcome: true)
 //componentDidMount()
--- helpers/apirequest.js
+++ helpers/apirequest.js
@@ -1,2 +1,4 @@
+//USE BLUEBIRD MODULE TO PROMISIFY THESE FUNCTIONS?
+
 //1. function pull a show from jambase API
 //2. function pull a restaurant from zomato API
